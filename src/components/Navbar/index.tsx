5be6c730a3555b267abcfc9599e0d95e89602c16
Romashka <GrebenevRV@gmail.com>
2025-02-11 10:10:00 +0300

    refactoring: navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,18 +3,16 @@
 import classNames from "classnames";
 import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
-import { Link, useLocation } from "react-router-dom"; // Используем хук для получения текущего пути
+import { Link, useLocation } from "react-router-dom";
 
 import style from "./index.module.scss";
 
-// Определяем интерфейс для ссылок
 interface TabLink {
   id: string;
   label: string;
   url: string;
 }
 
-// Массив ссылок
 const links: TabLink[] = [
   { id: "home", label: "Home", url: "/" },
   { id: "about", label: "About", url: "/about" },
@@ -22,12 +20,10 @@ const links: TabLink[] = [
   { id: "business", label: "Business", url: "/business" }
 ];
 
-// Компонент Navbar
 export const Navbar = () => {
-  const location = useLocation(); // Получаем текущий путь
+  const location = useLocation();
   const [activeLink, setActiveLink] = useState<string>("");
 
-  // Обновляем активную ссылку при изменении пути
   useEffect(() => {
     const currentTab =
       links.find((link) => link.url === location.pathname)?.id || links[0].id;
@@ -35,28 +31,29 @@ export const Navbar = () => {
   }, [location.pathname]);
 
   return (
-    <div className={style.tabsContainer}>
-      {links.map((link) => (
-        <Link
-          to={link.url}
-          key={link.id}
-          onClick={() => setActiveLink(link.id)}
-          className={classNames(
-            style.link,
-            activeLink === link.id && style.active
-          )}
-        >
-          {activeLink === link.id && (
-            <motion.span
-              layoutId="bubble"
-              className={style.bubble}
-              transition={{ type: "spring", bounce: 0.2, duration: 0.6 }}
-            />
-          )}
+    <div className={style.container}>
+      <div className={style.tabs}>
+        {links.map((link) => (
+          <Link
+            to={link.url}
+            key={link.id}
+            onClick={() => setActiveLink(link.id)}
+            className={classNames(style.link, {
+              [style.linkActive]: activeLink === link.id
+            })}
+          >
+            {activeLink === link.id && (
+              <motion.span
+                layoutId="bubble"
+                className={style.bubble}
+                transition={{ type: "spring", bounce: 0.2, duration: 0.6 }}
+              />
+            )}
 
-          {link.label}
-        </Link>
-      ))}
+            {link.label}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
