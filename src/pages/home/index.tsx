5422aa8784b05139966b1b98758e8ca64747d0ec
Romashka <GrebenevRV@gmail.com>
2025-02-11 11:27:46 +0300

    wip: slider

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,21 @@
-import { TypingText } from "./_components/TypingText";
+import type { EmblaOptionsType } from "embla-carousel";
 
-import "swiper/css";
-import "swiper/css/effect-coverflow";
+import { EmblaCarousel } from "components/Carousel/EmblaCarousel";
+
+import { TypingText } from "./_components/TypingText";
 
 import style from "./page.module.scss";
 
 export default function HomePage() {
+  const OPTIONS: EmblaOptionsType = { dragFree: true, loop: true };
+  const SLIDE_COUNT = 5;
+  const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
+
   return (
     <div className={style.page}>
       <TypingText />
+
+      <EmblaCarousel slides={SLIDES} options={OPTIONS} />
     </div>
   );
 }
